1bd5059304350a14347768e280beede2b672df58
Ceri Bain <cerimac@Ceris-MacBook-Pro-3.local>
2026-02-24 14:45:51 +0000

    fixes nesting bugs in App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
-    <>
-      <main>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<ArticleList />}>
-              <Route path="/articles" element={<ArticleList />}></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </main>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/articles" element={<ArticleList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
